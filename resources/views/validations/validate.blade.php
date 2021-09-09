792b990d3ad372abbc7d00036b34b09ac6f9fa52
oggie sutrisna <sutrisna.oggie@gmail.com>
2021-09-09 15:25:49 +0800

    images upload

---
+++ resources/views/validations/validate.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>
+      Success!
+</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap" rel="stylesheet">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="{{asset('assets/plugins/fontawesome-free/css/all.min.css')}}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{asset('assets/css/adminlte.min.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/plugins/sweetalert2/sweetalert2.min.css') }}">
+</head>
+<body class="hold-transition register page">
+    <div class="card card-body register-card-body card-outline card-primary">
+        <h2 align="center">Success</h2>
+        <hr />
+        <h2 align="center">Your input has been recorded to our server, please wait for a moment for interview</h2>
+    </div>
+
+</body>
+<!-- jQuery -->
+<script src="{{asset('assets/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('assets/js/adminlte.min.js')}}"></script>
+<script src="{{asset('assets/js/script.js')}}"></script>
+<script src="{{ asset('assets/plugins/sweetalert2/sweetalert2.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/dropzone/dropzone.js')}}"></script>
+</body>
+</html>
